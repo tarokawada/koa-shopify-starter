785b1504e2d4882e432227cef8865663f361cf1f
Taro Kawada <kawadataro@gmail.com>
2019-01-08 14:57:17 +0900

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,35 @@
 const {NODE_ENV} = process.env;
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const devMode = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  mode: NODE_ENV === 'production' ? NODE_ENV : 'development',
-  // the place webpack will start when building your bundles
-  entry: ['./client/index.js'],
-  // sets up rules for any special importers
+  mode: NODE_ENV === "production" ? NODE_ENV : "development",
+  entry: ["./client/index.js"],
+  plugins: [new MiniCssExtractPlugin({})],
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      {
         test: /\.css$/,
         use: [
-          { loader: "style-loader" },
-          { loader: "css-loader" }
-        ]
-      }
+          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
     ],
   },
   // file extensions for webpack to look at
   resolve: {
-    extensions: ['*','.mjs', '.js', '.jsx'],
+    extensions: ["*", ".mjs", ".js", ".jsx"],
   },
   // where webpack will output your finished bundle
   output: {
-    path: __dirname + '/dist',
-    publicPath: '/',
-    filename: 'bundle.js',
+    path: __dirname + "/dist",
+    publicPath: "/",
+    filename: "bundle.js",
   },
 };
